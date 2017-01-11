8807f3870511e24fba9d1cfb4a1e47a00d4b4914
Vladix97 <gramovv@gmail.com>
2017-01-11 21:56:38 +0200

    added recursive array sum

---
+++ 01.RecursionAndRecursiveAlgorithms/Exercises/src/p01_recursiveArraySum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class p01_recursiveArraySum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int arrayLength = Integer.parseInt(in.nextLine());
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            array[i] = in.nextInt();
+        }
+
+        int sumOfArray = getArraySum(array, arrayLength - 1);
+        System.out.println(sumOfArray);
+    }
+
+    private static int getArraySum(int[] array, int index) {
+        if (index == 0) {
+            return array[index];
+        } else {
+            return array[index] + getArraySum(array, index - 1);
+        }
+    }
+}
