d5b9e229806fdf56fdfb24dfa5b1374464fa094a
Vladix97 <gramovv@gmail.com>
2017-01-18 17:17:14 +0200

    create combinations without repetition iterative

--- 02.CombinatorialAlgorithms/CombinatorialAlgorithmsHomework/src/p02_permutationsIteratively.java
+++ 02.CombinatorialAlgorithms/CombinatorialAlgorithmsHomework/src/p02_permutationsIteratively.java
@@ -1,24 +1,45 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class p02_permutationsIteratively {
+    static int[] arr;
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int n = 3/*Integer.parseInt(in.nextLine())*/;
-        int[] arr = new int[n];
+        int n = Integer.parseInt(in.nextLine());
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = i + 1;
         }
+        
+        combWithoutRepetitionsIterative();
+    }
 
-        int index = n - 1;
-        while (true) {
-            for (int i = index + 1; i < n; i++) {
-                int current = arr[index];
-                arr[index] = arr[i];
+    public static void combWithoutRepetitionsIterative() {
+        System.out.println(Arrays.toString(arr));
+        int n = arr.length;
+        int[] p = new int[n];
+        int i = 1;
+        while (i < n) {
+            if (p[i] < i) {
+                int j = ((i % 2) == 0) ? 0 : p[i];
+                swap(arr, i, j);
 
+                System.out.println(Arrays.toString(arr));
+                p[i]++;
+                i = 1;
             }
-
-            index--;
+            else {
+                p[i] = 0;
+                i++;
+            }
         }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
